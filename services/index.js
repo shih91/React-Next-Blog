744e5173876df16a9da2740da50e6a5a65ef1c39
Shih Chiang <shih91@gmail.com>
2021-12-08 20:22:19 +0800

    newest blog post should be visible on top

--- services/index.js
+++ services/index.js
@@ -4,31 +4,31 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
     const query = gql`
-        query MyQuery {
-            postsConnection {
-            edges {
-                node {
-                author {
-                    bio
-                    name
-                    id
-                    photo {
-                    url
+        query getPosts {
+            postsConnection (orderBy: updatedAt_DESC) {
+                edges {
+                    node {
+                        author {
+                            bio
+                            name
+                            id
+                            photo {
+                            url
+                            }
+                        }
+                        createdAt
+                        slug
+                        title
+                        excerpt
+                        featuredImage {
+                            url
+                        }
+                        categories {
+                            name
+                            slug
+                        }
                     }
                 }
-                createdAt
-                slug
-                title
-                excerpt
-                featuredImage {
-                    url
-                }
-                categories {
-                    name
-                    slug
-                }
-                }
-            }
             }
         }
     `
